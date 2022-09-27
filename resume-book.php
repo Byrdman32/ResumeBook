53c82dbd43cfb419e3fc8e167e687a1bbfad98b1
Eli Byrd <edbgkk@mst.edu>
2022-09-27 00:54:26 -0500

    Create Header

---
+++ resume-book.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Plugin Name:     Resume Book
+ * Plugin URI:      https://projects.thebyrdspot.com/plugins/resume-book
+ * Description:     A custom WordPress Plugin to implement a resume book for the Missouri S&T Mars Rover Design Team.
+ * Version:         0.1
+ * Requires PHP:    7.2
+ * Author:          Eli Byrd
+ * Author URI:      https://thebyrdspot.com/about-me
+*/
